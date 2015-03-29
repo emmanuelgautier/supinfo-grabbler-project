79df3144973a141f40a4e2ff6bd64472acc6419c
Emmanuel Gautier <manu.gautier1394@gmail.com>
2015-03-29 17:28:50 +0200

    add actions handlers to routes

--- app/routes/actions.js
+++ app/routes/actions.js
@@ -5,9 +5,11 @@ var actions = require('../controllers/actions');
 var routes = [{
     method: 'GET',
     path: '/follow/{user}',
+    handler: actions.follow
   }, {
     method: 'GET',
     path: '/unfollow/{user}',
+    handler: actions.unfollow
   }
 ];
 
